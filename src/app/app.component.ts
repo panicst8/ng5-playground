dfb724b8b3bcc2c71487b0594b0c6aabad8e119f
Chris Milford <panicst8@Chriss-MacBook-Pro.local>
2017-11-06 11:01:36 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss'],
+   preserveWhitespaces: false,
+})
+export class AppComponent {
+   title = 'app';
+}
